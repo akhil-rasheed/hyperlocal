f349035b1234dedad310bd174b70a1b832d126f3
Akhil <akhilrasheed16@gmail.com>
2023-03-30 11:27:40 +0530

    Initialised frontend

--- backend-api/index.js
+++ backend-api/index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
   res.send(posts);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log("listening on port 3001");
+  console.log(`Server running on port ${PORT}`);
 });
---
+++ frontend/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="text-white text-lg font-bold font-sans flex flex-col w-full h-screen bg-black items-center justify-center"
+  >
+    <img src="~/assets/img/logo.png" />
+    <n-button>Start experiencing nearby</n-button>
+  </div>
+</template>
+
+<script>
+import { NButton } from "naive-ui";
+export default {
+  components: {
+    NButton,
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
